82c6a0c6c513a68a742f7015c21c7434c1520e24
Nilyass <47905754+Nilyass@users.noreply.github.com>
2019-05-11 20:52:58 +0000

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ Client.on("message", async (message) => {
 	if (command === "say") {
 		message.delete()
                 const embed = new Discord.RichEmbed()
-		.setColor(0x954D23)
+		.setColor(RANDOM)
 		.setDescription(args.join(" "));
 		message.channel.send({embed})
 	} else
