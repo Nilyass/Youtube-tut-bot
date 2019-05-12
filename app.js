2bc44d08731ee3de25bfae4ef97929a4098a7f04
Nilyass <47905754+Nilyass@users.noreply.github.com>
2019-05-12 15:01:05 +0000

    Update app.js

--- app.js
+++ app.js
@@ -50,7 +50,6 @@ Client.on("message", async (message) => {
 		   if (text.length < 1) return message.channel.send("Can not announce nothing");
 		   //const colour = args.slice(2).join("");
 		   const embed = new Discord.RichEmbed()
-		   .setColor("0x" + color)
 		   .setTitle("Important Announcement:")
 		   .setDescription(text);
 		   message.channel.send("@everyone")
